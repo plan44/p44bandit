4ebf2b08cef641d353c190e589608577c1838694
Lukas Zeller <luz@plan44.ch>
2019-11-17 21:16:59 +0100

    QCAD postprocessor: prevent linenumber-only lines, because these are GOTOs in BANDIT.
    Also cleaned up a bit, removed unneeded commented out sample code

--- QCAD-postprocessor/GCodeBandit.js
+++ QCAD-postprocessor/GCodeBandit.js
@@ -22,9 +22,9 @@ function GCodeBandit(cadDocumentInterface, camDocumentInterface) {
 
     // header / footer before / after output:
     this.header = [
-      "[N]&G99", // BANDIT: drive to machine zero
-      "[N] [Z_ZERO][Y1][X1]G92", // set position register (set workpiece zero)
-      "[N] G90" // absolute programming (G91 would be relative)
+        "[N]&G99", // BANDIT: drive to machine zero
+        "[N] [Z_ZERO][Y1][X1]G92", // set position register (set workpiece zero)
+        "[N] G90" // absolute programming (G91 would be relative)
     ];
     this.footer = [
         "[N] M2" // legacy end-of-program (instead of M30)
@@ -39,21 +39,23 @@ function GCodeBandit(cadDocumentInterface, camDocumentInterface) {
 
 
     // rapid moves:
-    //                    name,                  ID,                     always, prefix, decimals, options
-    this.registerVariable("xPosition",           "X_RAPID",              false, "I", "DEFAULT", "DEFAULT");
-    this.registerVariable("yPosition",           "Y_RAPID",              false, "J", "DEFAULT", "DEFAULT");
-    this.registerVariable("zPosition",           "Z_RAPID",              false, "K", "DEFAULT", "DEFAULT");
+    // - need representation of X/Y/Z as I/J/K for rapid moves (Bandit does not have G0/G1)
+    //                    name,        ID,        always, prefix, decimals,  options
+    this.registerVariable("xPosition", "X_RAPID", false,  "I",    "DEFAULT", "DEFAULT");
+    this.registerVariable("yPosition", "Y_RAPID", false,  "J",    "DEFAULT", "DEFAULT");
+    this.registerVariable("zPosition", "Z_RAPID", false,  "K",    "DEFAULT", "DEFAULT");
     this.rapidMove =                 "[N] [X_RAPID][Y_RAPID]";
-    this.rapidMoveZ =                "[N] [Z]";
+    this.rapidMoveZ =                "[N] [Z]"; // no rapid Z moves for now
 
     // linear moves:
+    // - Bandit does not have G1, all X/Y/Z moves are non-rapid linear moves
     this.firstLinearMove =           "[N] [X][Y]";
     this.linearMove =                "[N] [X][Y]";
 
     // linear lead in / out:
-    // these default to this.lineadMove:
-    this.linearLeadIn = undefined;
-    this.linearLeadOut = undefined;
+    // these default to this.linearMove:
+    this.linearLeadIn =              undefined;
+    this.linearLeadOut =             undefined;
 
     // linear Z moves:
     this.firstLinearMoveZ =          "[N] [Z]";
@@ -70,8 +72,6 @@ function GCodeBandit(cadDocumentInterface, camDocumentInterface) {
     this.arcCWMove =                 this.firstArcCWMove;
     this.firstArcCCWMove =           this.firstArcCWMove;
     this.arcCCWMove =                this.firstArcCWMove;
-
-
 }
 
 // Configuration is derived from GCodeBase:
@@ -80,6 +80,22 @@ GCodeBandit.prototype = new GCodeBase();
 // Display name shown in user interface:
 GCodeBandit.displayName = "G-Code (BANDIT) [mm]";
 
+
+/**
+ * Writes the given line (string + line ending) to the output stream.
+ */
+GCodeBandit.prototype.writeLine = function(line) {
+    // avoid linenumber-only lines under all circumstances
+    // (because these are GOTOs in BANDIT)
+    // Note: CamExporterV2 does have a check for that, but it does not work (probably due to spaces)
+    var rx = new RegExp("^N\\d+\\s*$", "g");
+    if (rx.test(line)) {
+        //debugger;
+        return;
+    }
+    // call base implemenation of writeLine:
+    return GCodeBase.prototype.writeLine.call(this, line);
+}
 
 
 /**
@@ -90,13 +106,11 @@ GCodeBandit.prototype.writeFile = function(fileName) {
     var banditZZero = this.getGlobalOption("BanditZZero", "100");
     this.banditZZero = Number(banditZZero);
 
-    this.registerVariable("banditZZero",         "Z_ZERO",              false, "Z", "DEFAULT", "DEFAULT");
+    this.registerVariable("banditZZero", "Z_ZERO", false, "Z", "DEFAULT", "DEFAULT");
 
     // call base implemenation of writeFile:
     return GCodeBase.prototype.writeFile.call(this, fileName);
 };
-
-
 
 
 /**
@@ -120,46 +134,5 @@ GCodeBandit.prototype.initConfigDialog = function(dialog) {
     leZZero.editable = true;
     leZZero.objectName = "BanditZZero";
     hBoxLayout.addWidget(leZZero, 0,0);
-
-
-    // label and combo box samples
-    /*
-    var hBoxLayout = new QHBoxLayout(null);
-    vBoxLayout.addLayout(hBoxLayout, 0);
-
-    var lLaserOn = new QLabel(qsTr("LASER on:"));
-    hBoxLayout.addWidget(lLaserOn, 0,0);
-
-    var cbLaserOn = new QComboBox();
-    cbLaserOn.editable = true;
-    cbLaserOn.objectName = "CamLaserOnCode";
-    cbLaserOn.addItem("M03");
-    cbLaserOn.addItem("S255 M03");
-    cbLaserOn.addItem("S255");
-    hBoxLayout.addWidget(cbLaserOn, 0,0);
-
-    hBoxLayout = new QHBoxLayout(null);
-    vBoxLayout.addLayout(hBoxLayout, 0);
-
-    var lLaserOff = new QLabel(qsTr("LASER off:"));
-    hBoxLayout.addWidget(lLaserOff, 0,0);
-
-    var cbLaserOff = new QComboBox();
-    cbLaserOff.editable = true;
-    cbLaserOff.objectName = "CamLaserOffCode";
-    cbLaserOff.addItem("M05");
-    cbLaserOff.addItem("S0 M05");
-    cbLaserOff.addItem("S0");
-    hBoxLayout.addWidget(cbLaserOff, 0,0);
-    */
 };
-
-
-
-/* this is how to ovveride methods (but we don't need it
-GCodeBandit.prototype.exportArc = function(arc) {
-    debugger; // debugger would halt here, allowing inspection of vars
-    CamExporterV2.prototype.exportArc.call(this, arc); // call "inherited"
-};
-*/
 
